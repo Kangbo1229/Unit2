5617f5ee1805d304ca8c9d3c50991d405e883a6b
kangbo1229 <kangbo@ssfs.org>
2019-09-27 12:56:48 -0400

    Completed PointsAndRectangles

---
+++ src/PointsAndRectangles.java
@@ -0,0 +1,22 @@
+import java.awt.*;
+import java.util.Scanner;
+
+public class PointsAndRectangles {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter the x coordinate: ");
+        int x = scan.nextInt();
+        System.out.print("Enter the y coordinate: ");
+        int y = scan.nextInt();
+        System.out.print("Enter the length: ");
+        int L = scan.nextInt();
+        System.out.print("Enter the width: ");
+        int w = scan.nextInt();
+        System.out.print("java.awt.Point "+"[x="+x+",y="+y+"]");
+
+
+
+    }
+}
