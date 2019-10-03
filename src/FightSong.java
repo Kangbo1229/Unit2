8be44660223ec0d8879bb8e1c89efad725a2fdd3
kangbo1229 <kangbo@ssfs.org>
2019-10-03 10:06:50 -0400

    Completed FightSong

---
+++ src/FightSong.java
@@ -0,0 +1,26 @@
+public class FightSong {
+
+    public static void chant() {
+        System.out.println("Go, team, go!\nYou can do it.");
+    }
+    public static void name() {
+        System.out.println("You're the best,\nIn the West.");
+    }
+    public static void blank() {
+        System.out.println(" ");
+    }
+
+    public static void main(String[]args) {
+        chant();
+        blank();
+        chant();
+        name();
+        chant();
+        blank();
+        chant();
+        name();
+        chant();
+        blank();
+        chant();
+    }
+}
