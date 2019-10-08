85b117d8ae2675b5ba280db68c42aa1b65352e1d
kangbo1229 <kangbo@ssfs.org>
2019-10-08 12:08:55 -0400

    Completed Names

---
+++ src/Names.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Names {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        String First = "Taehyung";
+        String Last = "kim";
+        int len = First.length();
+
+    }
+}
