7ec80d779da6cd58e45dad93186338855ad98826
kangbo1229 <kangbo@ssfs.org>
2019-10-10 15:29:04 -0400

    Working TheNameGame

---
+++ src/WrappersDelight.java
@@ -0,0 +1,13 @@
+public class WrappersDelight {
+    public static void main(String[] args) {
+        int num1 = 14;
+        Integer num2 = new Integer(8);
+        double num3 = 2.5;
+        Double num4 = num2+num3;
+        num2 = 5;
+        num3 = num1+num2+num4;
+
+
+
+    }
+}
