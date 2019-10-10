7b7344b77e7312962d291428ce13fbed04366230
kangbo1229 <kangbo@ssfs.org>
2019-10-10 14:53:37 -0400

    Completed TheNameGame

--- src/TheNameGame.java
+++ src/TheNameGame.java
@@ -1,6 +1,25 @@
-public class TheNameGame {
+import java.util.Scanner;
 
-    public static void first() {
-        System.out.print("Fifty");
+public class TheNameGame {
+    public static void Game (String name) {
+        System.out.println(" ");
+        System.out.println(name+" "+name+", "+"bo-"+"B"+name.substring(1));
+        System.out.println("Banana-fana fo-"+name);
+        System.out.println("Fee-fi-mo-Mifty");
+        System.out.println(name.toUpperCase());
+
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("What is your first name?");
+        String First = scan.next();
+        System.out.print("What is your last name?");
+        String Last = scan.next();
+        Game(First);
+        Game(Last);
+
+
+
     }
 }
