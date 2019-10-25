a9ab8df7186c10d7755d05f9564c48a8c7e19058
kangbo@ssfs.org <Elly122913!>
2019-10-25 18:26:15 -0400

    nkj

---
+++ src/SquareWordRoot.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class SquareWordRoot {
+    public static void main(String[] args){
+        System.out.println("Enter your word: ");
+        Scanner scan = new Scanner(System.in);
+        String four = scan.nextLine();
+        int value = (int) Math.sqrt(four.length());
+        
+
+
+    }
+
+}
